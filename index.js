3fcad42afee1660cfddf788854a30e3aca4ae44f
szolarisz <thomas@nyirszikszi.hu>
2022-01-07 13:12:35 +0100

    Stitikus állományok kezelésével kiegészített megoldás ( a példa miatt az images mappában, csak css file található)

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const adatok = require("./moduljaim/adatModul")
 const port=4444;
 
+const publicOptions = {
+    extensions: ['js', 'html', 'css']
+}
+
 const app = express();
+
+app.use( express.static('public', publicOptions) );
+app.use( express.static('images'));
 
 app.set('view engine', 'ejs');
 
